536e79dcf147d02b61f38dfb7308f4dc18cf4938
debangi <debangiofficial@gmail.com>
2022-05-05 21:24:40 +0530

    created heading and dropdown component for select country

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import { FormControl, MenuItem, Select } from '@mui/material';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className='app'>
-      <h1>Covid 19 Tracker</h1>
+      <div className='app__header'>
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className='app__dropdown' value='abc'>
+          <Select variant='outlined'>
+            {countries.map((country) => (
+              <MenuItem value={country.name}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
